48279d6296d697f1f7b9c83a29c069ddf3432dfd
Eric Lui <lui.eric.93@gmail.com>
2016-11-21 15:38:59 -0500

    Add jshint to the project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AmalgamateJS
 A framework that helps simplify bot creation across multiple platforms.
+
+This project has a pre-commit hook.
+As of right now it just applies jshint through gulp.
+If you believe that the jslint rule its breaking is okay, just commit with the -n option.
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('default', function() {
+  return gulp.src(['lib/**/*.js'])
+             .pipe(jshint())
+             .pipe(jshint.reporter('jshint-stylish'))
+             .pipe(jshint.reporter('fail'))
+});

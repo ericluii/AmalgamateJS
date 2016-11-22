2352e1915350c3bbd4d1f5a431718478af06e87c
Eric Lui <ealui@uwaterloo.ca>
2016-11-22 15:23:49 -0500

    Install ESDOC for documentation, use 'gulp docs'

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const jscs = require('gulp-jscs');
+const esdoc = require('gulp-esdoc');
 
 gulp.task('jshint', function() {
-  return gulp.src(['lib/**/*.js'])
+  return gulp.src(['./lib/**/*.js'])
              .pipe(jshint())
              .pipe(jshint.reporter('jshint-stylish'))
              .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('jscs', function() {
-  return gulp.src(['lib/**/*.js'])
+  return gulp.src(['./lib/**/*.js'])
              .pipe(jscs({fix: true}))
              .pipe(jscs.reporter())
              .pipe(gulp.dest('lib'))
              .pipe(jscs.reporter('fail'));
+});
+
+gulp.task('docs', function() {
+  return gulp.src('./lib')
+             .pipe(esdoc({ destination: "./docs" }));
 });
 
 gulp.task('default', function() {

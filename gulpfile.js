b6f313a81cbd3b2b1cb529dbda54dc59fd9fd5b2
Eric Lui <ealui@uwaterloo.ca>
2016-11-22 15:08:01 -0500

    Setup JSCS with node style rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const jscs = require('gulp-jscs');
 
-gulp.task('default', function() {
+gulp.task('jshint', function() {
   return gulp.src(['lib/**/*.js'])
              .pipe(jshint())
              .pipe(jshint.reporter('jshint-stylish'))
-             .pipe(jshint.reporter('fail'))
+             .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  return gulp.src(['lib/**/*.js'])
+             .pipe(jscs({fix: true}))
+             .pipe(jscs.reporter())
+             .pipe(gulp.dest('lib'))
+             .pipe(jscs.reporter('fail'));
+});
+
+gulp.task('default', function() {
+  gulp.start('jshint', 'jscs');
 });
